f05382682e890f76eff22afbbef33c7130c22378
Aysun Soylu <aysunsoylu@Aysun-Dizustu-Bilgisayar.local>
2024-11-21 16:31:21 +0300

    index html ve js dosyalarında değisiklikler yapildi

---
+++ app.js
@@ -0,0 +1,8 @@
+const formWrapper = document.querySelector(".form-wrapper"); //burada tek tek değerlerini yakalıyorum 
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttonWrapper= document.querySelector(".button-wrapper");
+const searchButton = document. querySelector("#searchButton");
+const clearButton = document. querySelector("#clearButton");
+const imageListWrapper= document. querySelector(".imageList-wrapper");
+
