331b8463253cd22d2708ea6f9631ac2651c42a06
Aysun Soylu <aysunsoylu@Aysun-Dizustu-Bilgisayar.local>
2024-11-29 12:50:47 +0300

    butonlarla ilgili css duzenlemeri yapildi

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ const imageListWrapper= document. querySelector(".imageList-wrapper");
 runEventListeners();
 function runEventListeners(){
     form.addEventListener("submit", search);
+    clearButton.addEventListener("click", clear);
+}
 
+function clear(){
+    searchInput.value = search.value="";//içine girilen degeri siliyor
+    // Array.from(imageListWrapper.children).forEach((child)=>child.remove())
+    imageListWrapper.innerHTML=""; //kısa yolla bu sekilde de temizleyebiliriz
 }
 
 function search(e){
     const value = searchInput.value.trim();
     // @RequestParam - Spring-Rest APİ
-    fetch(`https://api.unsplash.com/search/photos?query=${value}`,{ //dinamik olarak bir değer geçiyorum
+    fetch(`https://api.unsplash.com/search/photos?query=${value}`,
+        { //dinamik olarak bir değer geçiyorum
         method : "GET",
         headers : {
             Authorization : "Client-ID b9pwqUTYntouarOP6_lvcOy50UKyCIYc7kWALhRMGS4"
@@ -29,7 +36,7 @@ function search(e){
     })
     .catch((err)=>console.log(err));
 
-    e.preventDefault();//sayfa yüklenirken gitmesin diye kullanırız
+    e.preventDefault();//submit anında sayfanın yenilenmesini engellemiş oluyoruz.
 }
 
 function addImageToUI(url){
